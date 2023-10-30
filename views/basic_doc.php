ca5babac015de708c840dd6a066d11600f825c14
Nick Koole <nickkoole@hotmail.com>
2023-10-30 14:02:27 +0100

    #3 een dummy menu toegevoegd op basis van de vorige code

--- views/basic_doc.php
+++ views/basic_doc.php
@@ -37,11 +37,27 @@ class BasicDoc extends HtmlDoc {
     }
 
     private function showMenu() {
-        echo '<h2>Hier hoort een menu te staan</h2>';
+        $data['menu'] = array('home' => 'Home', 'about' => 'About', 'contact' => 'Contact', 'webshop' => 'Webshop');
+        if (false /*isUserLoggedIn()*/) {
+            $data['menu']['cart'] = "Winkelwagen";
+            $data['menu']['orders'] = "Bestellingen";
+            $data['menu']['logout'] = "Logout "; // . getLoggedInUserName();
+        } else {
+            $data['menu']['register'] = "Register";
+            $data['menu']['login'] = "Login";
+        }
+
+        echo '<ul class="nav">';        
+        foreach($data['menu'] as $page => $label) {
+            
+            //Dit geeft uiteraard nog geen correcte links
+            echo '<li><a href="index.php?page=' . $page . '">' . $label . '</a></li>';
+        }
+        echo '</ul><br>';
     }
 
     protected function showContent() {
-        echo '<p>Deze pagina is basic</p>';
+        echo '<p class="pagetext">Dit is de basispagina.</p>';
     }
 
     private function showFooter() {
