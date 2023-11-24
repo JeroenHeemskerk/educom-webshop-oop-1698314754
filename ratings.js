ae2f979f7a894fff5806cbecc27f37c64c2c9b76
Nick Koole <nickkoole@hotmail.com>
2023-11-24 15:02:08 +0100

    #16 rating wordt dynamisch per detailpagina opgehaald

--- ratings.js
+++ ratings.js
@@ -1,39 +1,39 @@
 $(document).ready(function(){
-
-
-    $(".star").click( function() {
-        const value = $(this).attr('data-value')
-        console.log('Value: ${ value }')
-
-        $(".star").removeClass('red');
-
-        //$('.star') returnt een array van de sterren
-        $('.star').each( (index, elem) => {
-            const itemValue = $(elem).attr('data-value')
-            if (itemValue <= value)  {
-                $(elem).addClass('red')
-            }
-        })
-    })
-
+    const starGroup = $(".starrating");
+    var productId = starGroup.data("product-id");
+    var url = "http://localhost/educom-webshop-oop-1698314754/index.php?page=ajax&action=averageRatingByProduct&productId=" + productId;
+    
     $.ajax({
-        //Er wordt nog enkel statisch de rating van productId 1 opgevraagd
-        url: "http://localhost/educom-webshop-oop-1698314754/index.php?page=ajax&action=averageRatingByProduct&productId=1",
+        url: url,
         method: "GET",
-        dataType: "json",
         success: function(data) {
             console.log(data);
+            var response = JSON.parse(data);
+            var rating = response.rating;
             
-            $('.star').each( (index, elem) => {
-                const itemValue = rating
-                if (itemValue <= value)  {
-                    $(elem).addClass('red')
+            starGroup.children('.star').each( (index, elem) => {
+                if ((index + 1) <= rating)  {                    
+                    $(elem).addClass('red');
                 }
             })
         },
         error: function(xhr, status, error) {
             console.error('Error:', status, error);
         }
+    })
 
+    $(".star").click( function() {
+        const value = $(this).attr('data-value');
+        console.log('Value: ${ value }');
+
+        $(".star").removeClass('red');
+
+        //$('.star') returnt een array van de sterren
+        $('.star').each( (index, elem) => {
+            const itemValue = $(elem).attr('data-value');
+            if (itemValue <= value)  {
+                $(elem).addClass('red');
+            }
+        })
     })
 });
